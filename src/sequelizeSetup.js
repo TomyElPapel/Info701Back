68e9b947e3f19eb3ad5fdd2e5bafba9ab0002d47
TomyElPapel <thomasburfinp@gmail.com>
2024-10-01 09:06:42 +0200

    setup env

--- src/sequelizeSetup.js
+++ src/sequelizeSetup.js
@@ -5,6 +5,7 @@ const path = require("path");
 
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
     host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     dialect: "mysql",
     logging: (msg) => {},
 });
